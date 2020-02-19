7a3386695cc751a55a45790693de8698c8fa8afd
승가리 <imsa1224@korea.ac.kr>
2020-02-19 21:19:12 +0900

    rock scissor and paper game setting

---
+++ WITH CLI/가위바위보/main.js
@@ -0,0 +1,3 @@
+import Vue from "vue";
+import rockScissorPaper from "./rockScissorPaper";
+new Vue(rockScissorPaper).$mount("#root");
---
+++ WITH CLI/가위바위보/rockScissorPaper.vue
@@ -0,0 +1,16 @@
+<template> </template>
+<script>
+export default {
+  data() {},
+  computed: {},
+  methods: {}
+};
+</script>
+<style scoped>
+/* 이 컴포넌트 안에서만 이 스타일을 하고 싶으면 scoped를 붙여줘! */
+#computer {
+  width: 142px;
+  height: 200px;
+  background-position: 0 0;
+}
+</style>
