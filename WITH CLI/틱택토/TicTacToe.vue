d2a0649d2f7dd3b126e4c217c27afc6361c105f3
승가리 <imsa1224@korea.ac.kr>
2020-02-20 18:19:13 +0900

    well what is this

--- WITH CLI/틱택토/TicTacToe.vue
+++ WITH CLI/틱택토/TicTacToe.vue
@@ -1,29 +1,37 @@
 <template>
-  <table-component :table-data="tableData"></table-component>
+  <div>
+    <div>{{ turn }}님의 턴입니다</div>
+    <table-component :table-data="tableData"></table-component>
+    <div v-if="winner">{{ winner }}님의 압도적 승리!</div>
+  </div>
 </template>
 <script>
+import TableComponent from "./TableComponent";
+import Vue from "vue";
 export default {
-  components: {},
+  components: { TableComponent },
   data() {
-    tableData: [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""]
-    ];
+    return {
+      tableData: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+      ],
+      turn: "O",
+      winner: ""
+    };
   },
   methods: {}
 };
 </script>
-<style scoped>
-.ball {
-  display: inline-block;
-  border: 1px solid gray;
-  border-radius: 50%;
+<style>
+table {
+  border-collapse: collapse;
+}
+td {
+  border: 1px solid black;
   width: 40px;
   height: 40px;
-  line-height: 40px;
-  font-size: 20px;
   text-align: center;
-  margin-right: 20px;
 }
 </style>
