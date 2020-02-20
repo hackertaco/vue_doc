70200e1c4d1bbfc24c5090da2b23955a725ff8de
승가리 <imsa1224@korea.ac.kr>
2020-02-20 16:36:41 +0900

    basic setting of tictactoe game

---
+++ WITH CLI/틱택토/TableComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <table>
+    <tr-component
+      v-for="(rowData, index) in tableData"
+      :row-data="rowData"
+      :row-index="index"
+      :key="(rowData, index)"
+    ></tr-component>
+  </table>
+</template>
+
+<script>
+import TrComponent from "./TrComponent";
+export default {
+  props: {
+    tableData: Array
+  },
+  components: {
+    TrComponent
+  }
+};
+</script>
